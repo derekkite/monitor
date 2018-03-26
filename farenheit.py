827a9d3b9807c6686d508eaa509965db7326a32f
Ian <derekkite@gmail.com>
2018-03-26 21:39:08 +0000

    update

---
+++ farenheit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+
+client = ModbusClient(method='rtu', port='/dev/ttyUSB0', timeout=1, stopbits = 1, bytesize = 8,  parity='N', baudrate= 9600)
+client.connect()
+
+client.write_register(22,0)
+
