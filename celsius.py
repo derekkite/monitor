16bf85083115ed15cde724d69a790d46b5af9711
wrigian586 <37812472+wrigian586@users.noreply.github.com>
2018-03-29 08:55:46 -0700

    Delete celsius.py
    cleanup

--- celsius.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from pymodbus.client.sync import ModbusSerialClient as ModbusClient
-
-client = ModbusClient(method='rtu', port='/dev/ttyUSB0', timeout=1, stopbits = 1, bytesize = 8,  parity='N', baudrate= 9600)
-client.connect()
-
-client.write_register(22,10)
-
